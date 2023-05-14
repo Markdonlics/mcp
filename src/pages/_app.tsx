57dc9119787e2967cb2da7eaa4bd6a6a13ff2d49
MPC <markwilliam01259@gmail.com>
2023-05-14 01:11:25 -0700

    ...

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 import type { AppProps } from "next/app";
 import "@/styles/app.css";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,11 @@ export default function App({ Component, pageProps }: AppProps) {
       <div className="my-20"></div>
       <Component {...pageProps} />
       <Footer />
+      <div className="relative">
+        <div className="fixed bottom-0 right-0 p-5 transform transition hover:-translate-y-2">
+          <Link href="https://wa.me/message/7N2ID7LCRUUNM1" target="_blank" aria-label="Chat on WhatsApp"><img alt="Chat on WhatsApp" className="w-56 h-auto" src="images/logo/WhatsAppButtonGreenSmall.png"/></Link>
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,11 +6,6 @@ const contact = () => {
   return (
     <>
     <Contact />
-      <div className="relative">
-        <div className="fixed bottom-0 right-0 p-5 transform transition hover:-translate-y-2">
-          <Link href="https://wa.me/message/7N2ID7LCRUUNM1" target="_blank" aria-label="Chat on WhatsApp"><img alt="Chat on WhatsApp" className="w-56 h-auto" src="images/logo/WhatsAppButtonGreenSmall.png"/></Link>
-        </div>
-      </div>
     </>
   );
 };
